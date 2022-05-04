11b97e0bfed5942f47ff19d438eaa53d9c96872c
pmoretal <pmoretal01@ceinmark.net>
2022-05-04 12:07:46 +0200

    Test Commit

--- src/Practica_Repaso.java
+++ src/Practica_Repaso.java
@@ -2,7 +2,7 @@
 public class Practica_Repaso {
 
 	public static void main(String[] args) {
-		System.out.println("Esto es la práctica de repaso");
+		System.out.println("Esto Es La Práctica De Repaso");
 		System.out.println("Pablo");
 
 	}
